bb525c8248f0a718149ceaf2d9819f42b9d47e3a
FifthElement5 <124865453+FifthElement5@users.noreply.github.com>
2026-06-03 10:39:26 +0000

    sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'licznik-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Robot instaluje się w pamięci i chowa pliki
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Robot pilnuje, żeby strona działała nawet bez internetu
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
